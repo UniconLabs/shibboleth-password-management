32700b74dc4e03ec6ac78994cffed984d1f49cb9
Dmitriy Kopylenko <dima767@gmail.com>
2020-05-21 13:57:05 -0400

    wip

--- src/main/java/net/unicon/iam/shibboleth/passwordreset/flow/PasswordResetInit.java
+++ src/main/java/net/unicon/iam/shibboleth/passwordreset/flow/PasswordResetInit.java
@@ -1,18 +1,42 @@
 package net.unicon.iam.shibboleth.passwordreset.flow;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shibboleth.idp.profile.AbstractProfileAction;
 import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.storage.StorageRecord;
+import org.opensaml.storage.StorageService;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 
+@Slf4j
+@AllArgsConstructor
 public class PasswordResetInit extends AbstractProfileAction {
+
+    private StorageService storageService;
 
     @Nonnull
     @Override
     protected Event doExecute(@Nonnull RequestContext springRequestContext, @Nonnull ProfileRequestContext profileRequestContext) {
         String token = springRequestContext.getExternalContext().getRequestParameterMap().get("token");
+        String storedToken = null;
+        log.info("Received token: [{}]", token);
+        try {
+            StorageRecord<String> storageRecord = this.storageService.read("passwordreset", "token");
+            if(storageRecord == null) {
+                return new Event(this, "error");
+            }
+            storedToken = storageRecord.getValue();
+        }
+        catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return new Event(this, "error");
+        }
+        log.info("Stored token is [{}]", storedToken);
+
         springRequestContext.getFlowScope().put("resetToken", token);
 
         return new Event(this, "success");
--- src/main/java/net/unicon/iam/shibboleth/passwordreset/flow/SendPasswordResetInstructions.java
+++ src/main/java/net/unicon/iam/shibboleth/passwordreset/flow/SendPasswordResetInstructions.java
@@ -7,10 +7,13 @@ import net.shibboleth.idp.profile.AbstractProfileAction;
 import net.unicon.iam.shibboleth.passwordreset.support.EmailProperties;
 import net.unicon.iam.shibboleth.passwordreset.support.EmailService;
 import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.storage.StorageService;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
@@ -21,10 +24,24 @@ public class SendPasswordResetInstructions extends AbstractProfileAction {
 
     private EmailService emailService;
 
+    private StorageService storageService;
+
     @Nonnull
     @Override
-    protected Event doExecute(@Nonnull RequestContext springRequestContext, @Nonnull ProfileRequestContext profileRequestContext) {
-        if(this.emailService.send(this.emailProperties, "test@email.com", "Hooray, that works!")) {
+    protected Event doExecute(@Nonnull RequestContext springRequestContext,
+                              @Nonnull ProfileRequestContext profileRequestContext) {
+        String token = UUID.randomUUID().toString();
+
+        try {
+            this.storageService.create("passwordreset", "token", token, null);
+        }
+        catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return new Event(this, "error");
+        }
+
+
+        if(this.emailService.send(this.emailProperties, "test@email.com", String.format("Here is your reset token: %s", token))) {
             return new Event(this, "success");
         }
         return new Event(this, "error");
