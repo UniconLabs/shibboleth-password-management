856bd48f6956cb724becce7e0cabc73779d8418d
chasegawa <chasegawa@unicon.net>
2021-11-05 15:25:08 -0700

    IAMSE-1437 Updated to: Shib 4.1.4, ldaptive 2.1.0, lombok 1.18.16, Spring 5.3.9 Refactored code to match library change updates Refactored test code to use embedded LDAP rather than depend on outside Docker setup.

--- src/main/java/net/unicon/iam/shibboleth/passwordreset/service/ldap/LdapPasswordManagementService.java
+++ src/main/java/net/unicon/iam/shibboleth/passwordreset/service/ldap/LdapPasswordManagementService.java
@@ -4,41 +4,51 @@ import lombok.extern.slf4j.Slf4j;
 import net.unicon.iam.shibboleth.passwordreset.service.BasePasswordManagementService;
 import net.unicon.iam.shibboleth.passwordreset.service.PasswordManagementService;
 import net.unicon.iam.shibboleth.passwordreset.support.ldap.LdapProperties;
-import net.unicon.iam.shibboleth.passwordreset.support.ldap.Ldaptive;
+import org.ldaptive.BindConnectionInitializer;
+import org.ldaptive.ConnectionConfig;
 import org.ldaptive.ConnectionFactory;
+import org.ldaptive.Credential;
+import org.ldaptive.DefaultConnectionFactory;
+import org.ldaptive.FilterTemplate;
 import org.ldaptive.LdapAttribute;
 import org.ldaptive.LdapEntry;
-import org.ldaptive.Response;
-import org.ldaptive.SearchFilter;
-import org.ldaptive.SearchResult;
+import org.ldaptive.LdapException;
+import static org.ldaptive.ResultCode.SUCCESS;
+import org.ldaptive.SearchOperation;
+import org.ldaptive.SearchRequest;
+import org.ldaptive.SearchResponse;
+import org.ldaptive.ad.extended.FastBindConnectionInitializer;
+import org.ldaptive.extended.ExtendedOperation;
+import org.ldaptive.extended.ExtendedResponse;
+import org.ldaptive.extended.PasswordModifyRequest;
+import org.ldaptive.handler.ResultPredicate;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 /**
  * Implementation of {@link PasswordManagementService} for LDAP directory backend.
  */
 @Slf4j
 public class LdapPasswordManagementService extends BasePasswordManagementService {
-
-    private final LdapProperties ldapProperties;
-
-    //Is ConnectionFactory thread-safe?
-    private final ConnectionFactory connectionFactory;
+    private ConnectionFactory connectionFactory;
+    private LdapProperties ldapProperties;
 
     public LdapPasswordManagementService(LdapProperties ldapProperties) {
         this.ldapProperties = ldapProperties;
-        this.connectionFactory = Ldaptive.connectionFactoryOf(ldapProperties);
+        buildConnectionFactory();
     }
 
     @Override
     public String findEmailAddressFor(String username) {
         Assert.notNull(username, "username cannot be null");
-        Optional<Response<SearchResult>> optionalResponse = findSearchResultFor(username, "mail");
+        Optional<SearchResponse> optionalResponse = findSearchResultFor(username, "mail");
         return optionalResponse
                 .map(r -> {
-                    LdapEntry e = r.getResult().getEntry();
+                    LdapEntry e = r.getEntry();
                     LdapAttribute attr = e == null ? null : e.getAttribute();
                     if (attr == null) {
                         log.warn("Cannot find 'mail' attribute for [{}]", username);
@@ -57,19 +67,71 @@ public class LdapPasswordManagementService extends BasePasswordManagementService
             log.warn("The LDAP entry is not found for [{}]. Unable to reset password", username);
             return false;
         }
-        return Ldaptive.executeGenericLdapPasswordResetOperation(connectionFactory, dn, newPassword);
+        return executeGenericLdapPasswordResetOperation(dn, newPassword);
     }
 
     //Package-private - friendly for unit testing
     String findDnFor(String username) {
-        Optional<Response<SearchResult>> optionalResponse = findSearchResultFor(username, "mail");
-        return optionalResponse
-                .map(r -> r.getResult().getEntry().getDn())
-                .orElse(null);
+        Optional<SearchResponse> optionalResponse = findSearchResultFor(username, "mail");
+        return optionalResponse.map(r -> r.getEntry().getDn()).orElse(null);
     }
 
-    private Optional<Response<SearchResult>> findSearchResultFor(String username, String... returnAttributes) {
-        SearchFilter filter = Ldaptive.searchFilterOf(ldapProperties.getSearchFilter(), "user", List.of(username));
-        return Ldaptive.executeSearchOperation(connectionFactory, ldapProperties.getBaseDn(), filter, returnAttributes);
+    Optional<SearchResponse> findSearchResultFor(String username, String... returnAttributes) {
+        FilterTemplate filter = buildFilter(ldapProperties.getSearchFilter(), "user", List.of(username));
+        return executeSearchOperation(ldapProperties.getBaseDn(), filter, returnAttributes);
+    }
+
+    private void buildConnectionFactory() {
+        ConnectionConfig config = new ConnectionConfig();
+        config.setLdapUrl(ldapProperties.getUrl());
+        if (StringUtils.substringMatch(ldapProperties.getBindDn(), 0, "*") &&
+            StringUtils.substringMatch(ldapProperties.getBindPassword(), 0, "*")) {
+            config.setConnectionInitializers(new FastBindConnectionInitializer());
+        } else if (StringUtils.hasText(ldapProperties.getBindDn()) && StringUtils.hasText(ldapProperties.getBindPassword())) {
+            config.setConnectionInitializers(new BindConnectionInitializer(ldapProperties.getBindDn(), new Credential(ldapProperties.getBindPassword())));
+        }
+        connectionFactory = new DefaultConnectionFactory(config);
+    }
+
+    FilterTemplate buildFilter(final String filterQuery, final String paramName, final List<String> params) {
+        FilterTemplate filter = new FilterTemplate();
+        filter.setFilter(filterQuery);
+        if (params != null) {
+            IntStream.range(0, params.size()).forEach(i -> {
+                if (filter.getFilter().contains("{" + i + '}')) {
+                    filter.setParameter(i, params.get(i));
+                } else {
+                    filter.setParameter(paramName, params.get(i));
+                }
+            });
+        }
+        log.debug("Constructed LDAP search filter [{}]", filter.format());
+        return filter;
+    }
+
+    private boolean executeGenericLdapPasswordResetOperation(String dn, String newPassword) {
+        log.debug("Executing password reset operation for DN: [{}]", dn);
+        try {
+            ExtendedOperation passwordModifyOperation = ExtendedOperation.builder().factory(connectionFactory).throwIf(ResultPredicate.NOT_SUCCESS).build();
+            ExtendedResponse response = passwordModifyOperation.execute(new PasswordModifyRequest(dn, null, newPassword));
+            log.debug("Password reset operation response code: [{}], message: [{}]", response.getResultCode(), response.getDiagnosticMessage());
+            return response.getResultCode() == SUCCESS;
+        }
+        catch (LdapException e) {
+            log.error(e.getMessage(), e);
+        }
+        return false;
+    }
+
+    private Optional<SearchResponse> executeSearchOperation(String baseDn, FilterTemplate searchFilter, String... returnAttributes) {
+        try {
+            SearchRequest req = new SearchRequest(baseDn, searchFilter, returnAttributes);
+            SearchOperation op = new SearchOperation(connectionFactory);
+            return Optional.ofNullable(op.execute(req));
+        }
+        catch (LdapException e) {
+            log.error(e.getMessage(), e);
+            return Optional.empty();
+        }
     }
 }
--- src/test/java/net/unicon/iam/shibboleth/passwordreset/service/ldap/LdapPasswordManagementServiceTests.java
+++ src/test/java/net/unicon/iam/shibboleth/passwordreset/service/ldap/LdapPasswordManagementServiceTests.java
@@ -1,23 +1,81 @@
 package net.unicon.iam.shibboleth.passwordreset.service.ldap;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
+import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.sdk.LDAPException;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.ldaptive.FilterTemplate;
+import org.ldaptive.SearchResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.URLDecoder;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-/**
- * These integration tests require Ldap Docker container with fixture data running.
- * Currently using 'directory' container from: https://github.com/UniconLabs/iam-learning-environment
- */
 @ContextConfiguration("/ldap-password-manager.xml")
 @ExtendWith(SpringExtension.class)
 public class LdapPasswordManagementServiceTests {
+    @Autowired private LdapPasswordManagementService ldapPasswordManagementService;
 
-    @Autowired
-    private LdapPasswordManagementService ldapPasswordManagementService;
+    private InetAddress bindAddress;
+    private String bindDSN = "cn=admin,dc=unicon,dc=local";
+    private String[] domainDsnArray = new String[] {"dc=unicon,dc=local"};
+    private List<String> ldifs = new ArrayList<>();
+    private InMemoryDirectoryServer testLdapServer;
+
+    public LdapPasswordManagementServiceTests() {
+        ldifs.add("testusers.ldif");
+        try {
+            this.bindAddress = InetAddress.getByName("127.0.0.1");
+        } catch (UnknownHostException e) {
+            throw new IllegalArgumentException(String.format("Unknown host address \"%s\"", "127.0.0.1"), e);
+        }
+    }
+
+    @BeforeEach
+    public void startEmbeddedLdap() throws LDAPException {
+        testLdapServer = createServer();
+        testLdapServer.startListening();
+    }
+
+    @AfterEach
+    public void stopEmbeddedLdap() {
+        testLdapServer.shutDown(true);
+    }
+
+    @Test
+    public void validateChangePassword() throws LDAPException {
+        Optional<SearchResponse> originalPass = ldapPasswordManagementService.findSearchResultFor("banderson", "userPassword");
+        assertEquals("password", originalPass.map(r -> r.getEntry().getAttribute("userPassword").getStringValue()).orElse(null));
+
+        boolean passwordChanged = ldapPasswordManagementService.resetPasswordFor("banderson", "foobar");
+        assertTrue(passwordChanged);
+
+        Optional<SearchResponse> updatedPass = ldapPasswordManagementService.findSearchResultFor("banderson", "userPassword");
+        assertEquals("foobar", updatedPass.map(r -> r.getEntry().getAttribute("userPassword").getStringValue()).orElse(null));
+    }
+
+    @Test
+    public void verifyCreateSearchFilter() throws LDAPException {
+        FilterTemplate searchFilter = ldapPasswordManagementService.buildFilter("(uid={user})", "user", List.of("kwhite"));
+        String format = searchFilter.format();
+        assertEquals("(uid=kwhite)", format);
+    }
 
     @Test
     public void verifyCorrectWiring() {
@@ -25,14 +83,40 @@ public class LdapPasswordManagementServiceTests {
     }
 
     @Test
-    public void verifySearchForEmail() {
-        String emailAddressForDimaInLdap = ldapPasswordManagementService.findEmailAddressFor("banderson");
-        assertEquals("banderson@mail.com", emailAddressForDimaInLdap);
+    public void verifyFindDn() throws LDAPException {
+        String dn = ldapPasswordManagementService.findDnFor("banderson");
+        assertEquals("uid=banderson,ou=People,dc=unicon,dc=local", dn);
     }
 
     @Test
-    public void verifyFindDn() {
-        String dn = ldapPasswordManagementService.findDnFor("banderson");
-        assertEquals("uid=banderson,ou=People,dc=unicon,dc=local", dn);
+    public void verifySearchForEmail() throws LDAPException {
+        String emailFromLdap = ldapPasswordManagementService.findEmailAddressFor("banderson");
+        assertEquals("banderson@mail.com", emailFromLdap);
+    }
+
+    private InMemoryDirectoryServerConfig createInMemoryServerConfiguration() {
+        try {
+            final InMemoryDirectoryServerConfig inMemoryDirectoryServerConfig = new InMemoryDirectoryServerConfig(domainDsnArray);
+            inMemoryDirectoryServerConfig.addAdditionalBindCredentials(bindDSN, "admin");
+            InMemoryListenerConfig listenerConfig = InMemoryListenerConfig.createLDAPConfig("test-listener", bindAddress, 8389, null);
+            inMemoryDirectoryServerConfig.setListenerConfigs(listenerConfig);
+            inMemoryDirectoryServerConfig.setSchema(null);
+            return inMemoryDirectoryServerConfig;
+        }
+        catch (LDAPException e) {
+            throw new IllegalStateException("Could not create configuration for the in-memory LDAP instance due to an exception", e);
+        }
+    }
+
+    private InMemoryDirectoryServer createServer() throws LDAPException {
+        final InMemoryDirectoryServer ldapServer = new InMemoryDirectoryServer(createInMemoryServerConfiguration());
+        for (final String ldif : ldifs) {
+            try {
+                ldapServer.importFromLDIF(false, URLDecoder.decode(Resources.getResource(ldif).getPath(), Charsets.UTF_8.name()));
+            } catch (UnsupportedEncodingException e) {
+                throw new IllegalStateException("Can not URL decode path:" + Resources.getResource(ldif).getPath(), e);
+            }
+        }
+        return ldapServer;
     }
 }
